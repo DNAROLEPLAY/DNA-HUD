a524e91155c9a51bb0773eec7342eb2d0d7466f9
DNA Role Play <dnarpbusiness@gmail.com>
2024-08-31 19:55:15 -0400

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Shah'
+description 'Custom HUD for Server'
+version '1.0.0'
+
+client_scripts {
+    'client/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css',
+    'html/logo.png',
+    'html/cash_icon.png',
+    'html/bank_icon.png',
+    'html/job_icon.png',
+    'html/id_icon.png'  -- Add the new ID icon
+}
